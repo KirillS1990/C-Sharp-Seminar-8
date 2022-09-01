07a8225ae4d31141783d8b373f24c489af7b565e
Kirill_s <skorkin.kirill@gmail.com>
2022-09-01 19:49:14 +0300

    DZ complete

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -81,68 +81,68 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-Console.Clear();
-Console.WriteLine($"Введите размер массива m x n и диапазон случайных значений:");
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
+// Console.Clear();
+// Console.WriteLine($"Введите размер массива m x n и диапазон случайных значений:");
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int range = InputNumbers("Введите диапазон: от 1 до ");
+// int[,] array = new int[m, n];
+// CreateArray(array);
+// WriteArray(array);
 
-int minSumLine = 0;
-int sumLine = SumLineElements(array, 0);
-for (int i = 1; i < array.GetLength(0); i++)
-{
-  int tempSumLine = SumLineElements(array, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}
-Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+// int minSumLine = 0;
+// int sumLine = SumLineElements(array, 0);
+// for (int i = 1; i < array.GetLength(0); i++)
+// {
+//   int tempSumLine = SumLineElements(array, i);
+//   if (sumLine > tempSumLine)
+//   {
+//     sumLine = tempSumLine;
+//     minSumLine = i;
+//   }
+// }
+// Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
 
 
-int SumLineElements(int[,] array, int i)
-{
-  int sumLine = array[i,0];
-  for (int j = 1; j < array.GetLength(1); j++)
-  {
-    sumLine += array[i,j];
-  }
-  return sumLine;
-}
+// int SumLineElements(int[,] array, int i)
+// {
+//   int sumLine = array[i,0];
+//   for (int j = 1; j < array.GetLength(1); j++)
+//   {
+//     sumLine += array[i,j];
+//   }
+//   return sumLine;
+// }
 
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
 
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
+// void CreateArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(range);
+//     }
+//   }
+// }
 
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i,j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
+// void WriteArray (int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i,j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
@@ -151,45 +151,6 @@ void WriteArray (int[,] array)
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-
-// Console.Clear();
-// Console.WriteLine($"Введите размеры матриц и диапазон случайных значений:");
-// int m = InputNumbers("Введите число строк 1-й матрицы: ");
-// int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
-// int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
-// int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
-
-// int[,] firstMartrix = new int[m, n];
-// CreateArray(firstMartrix);
-// Console.WriteLine($"\nПервая матрица:");
-// WriteArray(firstMartrix);
-
-// int[,] secomdMartrix = new int[n, p];
-// CreateArray(secomdMartrix);
-// Console.WriteLine($"\nВторая матрица:");
-// WriteArray(secomdMartrix);
-
-// int[,] resultMatrix = new int[m,p];
-
-// MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-// Console.WriteLine($"\nПроизведение первой и второй матриц:");
-// WriteArray(resultMatrix);
-
-// void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
-// {
-//   for (int i = 0; i < resultMatrix.GetLength(0); i++)
-//   {
-//     for (int j = 0; j < resultMatrix.GetLength(1); j++)
-//     {
-//       int sum = 0;
-//       for (int k = 0; k < firstMartrix.GetLength(1); k++)
-//       {
-//         sum += firstMartrix[i,k] * secomdMartrix[k,j];
-//       }
-//       resultMatrix[i,j] = sum;
-//     }
-//   }
-// }
 
 // int InputNumbers(string input)
 // {
@@ -229,22 +190,6 @@ void WriteArray (int[,] array)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-
-// Console.Clear();
-// Random rnd = new Random((new Random()).Next());
-// int[,,] matrix = new int[2,2,2];
-// for (int i = 0; i < matrix.GetLength(0); i++)   
-// {   
-
-//     for (int j = 0; j < matrix.GetLength(1); j++)   
-//     {
-//         for (int k = 0; k < matrix.GetLength(1); k++)
-//         {
-//         Console.Write($"{matrix[i, j, k] = new Random().Next(10, 99)} ({i}, {j}, {k})\t");
-//         }
-//     Console.WriteLine();
-//     }
-// }
 
 // Console.Clear();
 // Console.WriteLine($"Введите размер массива X x Y x Z:");
